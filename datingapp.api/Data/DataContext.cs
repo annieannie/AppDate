a4ecca9b3bec0dc0e3066f9c36529d51fd2082e6
AZ <annie01slc@gmail.com>
2018-12-21 12:57:54 -0500

    Initial commit

---
+++ datingapp.api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using datingapp.api.Models;
+
+namespace datingapp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base (options)
+        {          
+        }
+        public DbSet<Value> Values  {get;set;} 
+    }
+}
