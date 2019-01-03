86ad267419e07c2fa60d81497c309fd2c45b8683
AZ <annie01slc@gmail.com>
2019-01-02 22:35:13 -0500

    End of section 7

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(public authService: AuthService, private alertify: AlertifyService,
+    private router:Router) { }
+  canActivate():  boolean {
+    if(this.authService.loggedIn())
+   { return true;}
+   this.alertify.error("access denied");
+   this.router.navigate(['/home']);
+  }
+}
+ 
